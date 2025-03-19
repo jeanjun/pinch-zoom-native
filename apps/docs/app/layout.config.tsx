1095204c42b9c02cf4581826b9a3a17ad230a77d
jeanjun <jjeanjun0@gmail.com>
2025-03-19 23:53:52 +0900

    chore(docs): docs title

--- apps/docs/app/layout.config.tsx
+++ apps/docs/app/layout.config.tsx
@@ -12,7 +12,7 @@ export const baseOptions: BaseLayoutProps = {
         >
           <circle cx={12} cy={12} r={12} fill="currentColor" />
         </svg>
-        zoom
+        pinch-zoom-native
       </>
     )
   }
